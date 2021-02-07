976b9446ce39981589aabe7a041e96811da77af2
yrd123 <yash.deorah@somaiya.edu>
2021-02-07 23:57:53 +0530

    GS

--- ccapp/gCloud.py
+++ ccapp/gCloud.py
@@ -5,12 +5,12 @@ from urllib.parse import urljoin
 
 
 class GoogleCloudMediaFileStorage(GoogleCloudStorage):
-       """
-         Google file storage class which gives a media file path from       MEDIA_URL not google generated one.
-       """
-        bucket_name = setting('GS_BUCKET_NAME')
-        def url(self, name):
-            """
-            Gives correct MEDIA_URL and not google generated url.
-            """
-            return urljoin(settings.MEDIA_URL, name)
+    """
+        Google file storage class which gives a media file path from       MEDIA_URL not google generated one.
+    """
+    bucket_name = setting('GS_BUCKET_NAME')
+    def url(self, name):
+        """
+        Gives correct MEDIA_URL and not google generated url.
+        """
+        return urljoin(settings.MEDIA_URL, name)
